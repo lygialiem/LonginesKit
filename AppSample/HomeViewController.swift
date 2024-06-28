c1d993e182b04ffdf2970e44681d950a8737e8f9
liam <>
2024-06-29 00:27:35 +0700

    update

--- AppSample/HomeViewController.swift
+++ AppSample/HomeViewController.swift
@@ -13,13 +13,5 @@ class HomeViewController: LKBaseViewController {
  
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
-        let remoteConfigPlugin = LKPluggableTool.queryAppDelegate(for: LKRemoteConfigPluggable.self)
-        remoteConfigPlugin?.didActiveO
-            .filter{$0}
-            .sink(receiveValue: { isActived in
-                
-            })
     }
 }
