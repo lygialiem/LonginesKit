4aea92d95a2979ea15fc3b28b505d917dcdf825a
abc <liam@L-I-A-M-MP14.local>
2024-10-23 16:10:58 +0700

    add isTrial

--- LonginesKit/Qonversion/LKIAPModel.swift
+++ LonginesKit/Qonversion/LKIAPModel.swift
@@ -4,4 +4,11 @@ import Qonversion
 public struct LKIAPModel {
     
     public let qonversonProduct: Qonversion.Product
+    
+    public var isTrial: Bool {
+        guard let trialPeriod = qonversonProduct.trialPeriod, trialPeriod.unitCount != 0 else {
+            return false
+        }
+        return true
+    }
 }
